b8198f6bd30f798b1b48891e7ca26ce83c0b5028
Felipe Delgado <928222224@telefonica.net>
2013-12-30 20:30:20 +0000

    Realiza cueta atrás de los contadores

--- Mide_distancia/Mide_distancia.ino
+++ Mide_distancia/Mide_distancia.ino
@@ -8,6 +8,9 @@
     #define coche 1
     #define no_coche 0
     
+    /****************************************/
+    /* Estructura para controlar cada coche */
+    /****************************************/
     struct sensor_coche
     {
       int trigger_pin;
@@ -15,20 +18,28 @@
       boolean estado; //0 - no hay coche  1 si hay
       byte filtro;  //para detectar flancos coche/no coche
       float umbral;  // distancia máxima que indica que hay coche
+      byte vueltas;
     };    
     
     struct sensor_coche s_coche1;
     struct sensor_coche s_coche2;
     
+    /*************************************/
+    /* Función para inicializar sensores */
+    /*************************************/
     void inicializa_sensor(struct sensor_coche * sc, int trigger_pin, int echo_pin)
     {
-      sc->estado = 0;
+      sc->estado = no_coche;
       sc->filtro = 255;
       sc->umbral = 600.0; // Al imprlementar puede que haya que diferenciar un coche de otro
       sc->trigger_pin = trigger_pin;
       sc->echo_pin = echo_pin;
+      sc->vueltas = 20;
     }
     
+    /***********/
+    /* Setup() */
+    /***********/
     void setup()
     {
       Serial.begin(9600);
@@ -40,7 +51,9 @@
       inicializa_sensor(& s_coche2,trigger2,echo2);
     }
     
-
+    /***********************/
+    /* Lectura de sensores */
+    /***********************/
     float leeSensor(struct sensor_coche * s_coche)
     { 
       digitalWrite(s_coche->trigger_pin,LOW);
@@ -49,29 +62,51 @@
       digitalWrite(s_coche->trigger_pin,HIGH);
       delayMicroseconds(10);
       digitalWrite(s_coche->trigger_pin,LOW);
-      // distance=pulseIn(eco,HIGH); // Medimos el ancho del pulso (microsegundos) 
+      // dMedimos el ancho del pulso (microsegundos) 
       return pulseIn(s_coche->echo_pin,HIGH); //*0.0001657;  distancia en metros  (*0.0001657)
     }
+    
+    /*******************************/
+    /* Detecta el paso de un coche */
+    /*******************************/
+    void detecta_coche(struct sensor_coche * sensor)
+    {
+      float distancia = leeSensor(sensor);  // OJO al puntero
+      byte flag =  (distancia < sensor->umbral) ? 1 : 0; // 1 - está pasando un coche / 0 - no hay coche      sensor->filtro <<= 1;
+      // Al valor del filtro, se le suma el flag
+//      Serial.println(distancia);
+      sensor->filtro <<= 1;
+      sensor->filtro += flag;
+      sensor->filtro &= 0x7;
+          
+      if((sensor->filtro  == 0) && (sensor->estado == coche))
+      {
+          sensor->vueltas -= 1;
+          Serial.println(sensor->vueltas);
+      }
+      if(sensor->filtro  == 0)
+          sensor->estado = no_coche; 
+      else if(sensor->filtro  == 7)
+          sensor->estado = coche;
+          
+    }
      
+    /*********************/
+    /* Función principal */
+    /*********************/
     void loop()
     {
-      float distancia = leeSensor(& s_coche1);
-      byte flag =  (distancia < s_coche1.umbral) ? 1 : 0;
-      // Enviamos los datos medidos a traves del puerto serie      
-      Serial.println(distancia);
-      s_coche1.filtro <<= 1;
-      s_coche1.filtro += flag;
-      s_coche1.filtro &= 0x7;
-      if(s_coche1.filtro  == 7)
-        s_coche1.estado = coche;
-      else if(s_coche1.filtro  == 0)
-        s_coche1.estado = no_coche;
-
-      Serial.print("Filtro: ");
-      Serial.println(s_coche1.filtro); 
-      Serial.print("Estado: ");
+      detecta_coche(& s_coche1);
+//      detecta_coche(& s_coche2);
+      
+ /*     
+      Serial.print("Filtro 1: ");
+      Serial.println(s_coche1.filtro);
+      Serial.print("Estado 1: ");
       Serial.println(s_coche1.estado);
-//      Serial.println(leeSensor(& s_coche2));
-      //delay(100);
-      delay(1000);
+      Serial.print("Vueltas 1: ");
+      Serial.println(s_coche1.vueltas);
+ */     
+      delay(10);
+      //delay(2000);
     }
